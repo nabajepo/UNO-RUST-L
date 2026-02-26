0f83b39d6a7daa57ec1010e279b63a2d2e4ebeec
nabajepo <nabajepo@gmail.com>
2026-02-26 09:56:20 -0500

    Update

--- UNO-GAME/target/release/deps/windows_link-9f7b77db8bf1e12f.d
+++
@@ -1,8 +0,0 @@
-C:\Users\NGABIRE LEA\OneDrive\Documents\GitHub\UNO-RUST-L\UNO-GAME\target\release\deps\windows_link-9f7b77db8bf1e12f.d: C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\lib.rs C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\../readme.md
-
-C:\Users\NGABIRE LEA\OneDrive\Documents\GitHub\UNO-RUST-L\UNO-GAME\target\release\deps\libwindows_link-9f7b77db8bf1e12f.rlib: C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\lib.rs C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\../readme.md
-
-C:\Users\NGABIRE LEA\OneDrive\Documents\GitHub\UNO-RUST-L\UNO-GAME\target\release\deps\libwindows_link-9f7b77db8bf1e12f.rmeta: C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\lib.rs C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\../readme.md
-
-C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\lib.rs:
-C:\Users\NGABIRE\ LEA\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.0\src\../readme.md:
